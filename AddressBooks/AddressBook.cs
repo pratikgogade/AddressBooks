133dfc60ffd4c887de0456824dac1f209d3f6ff2
unknown <Pratik@Example.com>
2022-12-09 09:28:30 +0530

    [Pratik]Add:Display A Contact

--- AddressBooks/AddressBook.cs
+++ AddressBooks/AddressBook.cs
@@ -1,17 +1,19 @@
-﻿using System;
+﻿using AddressBooks;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AddressBooks
+namespace AddressBook
 {
-    internal class AddressBook
+    internal class Addressbook
     {
+        List<Contact> data = new List<Contact>();
         public void CreateContact()
         {
             Contact contact = new Contact();
-            Console.WriteLine("Enter FirstName");
+            Console.WriteLine("Enter Firstname");
             contact.Firstname = Console.ReadLine();
             Console.WriteLine("Enter Lastname");
             contact.Lastname = Console.ReadLine();
@@ -22,11 +24,28 @@ namespace AddressBooks
             Console.WriteLine("Enter State");
             contact.State = Console.ReadLine();
             Console.WriteLine("Enter Zip");
-            contact.Zip = Convert.ToInt16(Console.ReadLine());
-            Console.WriteLine("Enter Phonenumber");
+            contact.Zip = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter PhoneNumber");
             contact.PhoneNumber = Convert.ToInt64(Console.ReadLine());
             Console.WriteLine("Enter Email");
             contact.Email = Console.ReadLine();
+            data.Add(contact);
+        }
+        public void DisplayContact()
+        {
+            foreach (Contact records in data)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("*******Contact*******");
+                Console.WriteLine("Firstname" + records.Firstname);
+                Console.WriteLine("Lastname" + records.Lastname);
+                Console.WriteLine("Address" + records.Address);
+                Console.WriteLine("City" + records.City);
+                Console.WriteLine("State" + records.State);
+                Console.WriteLine("Zip" + records.Zip);
+                Console.WriteLine("PhoneNumber" + records.PhoneNumber);
+                Console.WriteLine("Email" + records.Email);
+            }
         }
     }
 }
--- AddressBooks/Program.cs
+++ AddressBooks/Program.cs
@@ -1,14 +1,27 @@
-﻿using AddressBooks;
-using System;
+﻿using System;
 
-namespace AddressBooks
+namespace AddressBook
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            AddressBook book = new AddressBook();
+            Addressbook book = new Addressbook();
             book.CreateContact();
+            while (true)
+            {
+                Console.WriteLine("1. Create Contact\n2. Display Contact\n3. Exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        book.CreateContact();
+                        break;
+                    case 2:
+                        book.DisplayContact();
+                        break;
+                }
+            }
         }
     }
 }
